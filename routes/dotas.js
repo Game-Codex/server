b002108f55efdd813cc19a0b51551bcfa0dc7c8d
Alif Taufik <alif.tr.aminuddin@gmail.com>
2019-11-29 13:56:39 +0700

    Remove Controller example files. Add routes for role-specific search for dota controllers

--- routes/dotas.js
+++ routes/dotas.js
@@ -2,5 +2,7 @@ const dotas = require('express').Router()
 const { DotaController } = require('../controllers')
 
 dotas.get('/', DotaController.getAllHeroes)
+dotas.get('/roles/:role', DotaController.getHeroesByRoles)
+dotas.get('/:id', DotaController.getHeroDetail)
 
 module.exports = dotas
