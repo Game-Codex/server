0ccbd8f27e426b78b2d015fe8277528773715b5f
edwinsatya <edwinsatyayudistira@gmail.com>
2019-11-29 14:59:45 +0700

    update server yugioh

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const loginGoogleRouter = require('./loginGoogle')
 const registerRouter = require('./registerRouter')
 const loginRouter = require('./login')
 const auth = require('../middlewares/auth')
+const yugiohRouter = require('./yugioh')
 
 routes.use('/loginGoogle', loginGoogleRouter)
 routes.use('/register', registerRouter)
@@ -11,6 +12,7 @@ routes.use('/login', loginRouter)
 
 routes.use(auth)
 routes.use('/pokemon', PokemonRouter)
+routes.use('/yugioh', yugiohRouter)
 routes.use('/dota', require('./dotas'))
 
 module.exports = routes
